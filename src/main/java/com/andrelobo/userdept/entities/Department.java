b76fe427150825efd34921d861d4d68f1e5ade9c
AndreLobo <loboandre55@gmail.com>
2024-09-27 12:32:25 -0300

    Create a new class Department

---
+++ src/main/java/com/andrelobo/userdept/entities/Department.java
@@ -0,0 +1,30 @@
+package com.andrelobo.userdept.entities;
+
+public class Department {
+	private Long id;
+	private String name;
+
+	public Department() {
+	}
+
+	public Department(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
