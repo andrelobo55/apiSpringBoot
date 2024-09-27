d20d42801ddcdb65e001cb7463301c4df4f7eadf
AndreLobo <loboandre55@gmail.com>
2024-09-27 12:56:36 -0300

    Adding auto increment for Department id in database

--- src/main/java/com/andrelobo/userdept/entities/Department.java
+++ src/main/java/com/andrelobo/userdept/entities/Department.java
@@ -1,11 +1,16 @@
 package com.andrelobo.userdept.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table (name = "tb_department")
 public class Department {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 
